d93149b9b3d42b83a1636ebd8c0fb4c31fe59b23
Tanja <tanja120a@gmail.com>
2023-06-09 07:01:15 +0200

    Section03-34 - Chaining commands

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -6,13 +6,12 @@ describe('contact form', () => {
         cy.get('[data-cy="contact-input-message"]').type('Hello World!');
         cy.get('[data-cy="contact-input-name"]').type('tanja');
         cy.get('[data-cy="contact-input-email"]').type('tanja120a@gmail.com');
-        cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
-        //cy.get('[data-cy="contact-btn-submit"]').should('be.enabled');
-        cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled');
-        cy.get('[data-cy="contact-btn-submit"]').click();
-        cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
-        //cy.get('[data-cy="contact-btn-submit"]').should('be.disabled');
-        cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+        cy.get('[data-cy="contact-btn-submit"]')
+        .contains('Send Message')
+        .and('not.have.attr', 'disabled');
+        cy.get('[data-cy="contact-btn-submit"]').click()
+        .contains('Sending...')
+        .should('have.attr', 'disabled');
         
     })
 })
