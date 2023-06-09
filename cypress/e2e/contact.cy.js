39225a2253bcfd0aa0ade9616762fe174541bf1e
Tanja <tanja120a@gmail.com>
2023-06-09 07:19:55 +0200

    Section03-36 - Using then()

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -6,13 +6,16 @@ describe('contact form', () => {
         cy.get('[data-cy="contact-input-message"]').type('Hello World!');
         cy.get('[data-cy="contact-input-name"]').type('tanja');
         cy.get('[data-cy="contact-input-email"]').type('tanja120a@gmail.com');
-        cy.get('[data-cy="contact-btn-submit"]')
-        .contains('Send Message')
-        .and('not.have.attr', 'disabled');
-        const btn = cy.get('[data-cy="contact-btn-submit"]');
-        btn.click()
-        btn.contains('Sending...')
-        btn.should('have.attr', 'disabled');
-        
-    })
-})
+        cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+        expect(el.attr('disabled')).to.be.undefined;
+        expect(el.text()).to.eq('Send Message');
+        });
+        //cy.get('[data-cy="contact-btn-submit"]')
+        //.contains('Send Message')
+        //.and('not.have.attr', 'disabled');
+        const btn = cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+        cy.get('@submitBtn').click()
+        cy.get('@submitBtn').contains('Sending...')
+        cy.get('@submitBtn').should('have.attr', 'disabled');        
+    });
+});
