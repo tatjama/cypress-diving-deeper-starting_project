8a0ddef650e7216ef568079229cb859fe5529996
Tanja <tanja120a@gmail.com>
2023-06-10 23:40:42 +0200

    Section03-46 - Using should insted thn

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -31,21 +31,17 @@ describe('contact form', () => {
     cy.get('[data-cy="contact-input-message"]').blur();
     cy.get('[data-cy="contact-input-message"]')
       .parent()
-      .then((el) => {
-        expect(el.attr('class')).to.contains('invalid');
-      });
+      .should('have.attr', 'class').and('match', /invalid/);
     
     cy.get('[data-cy="contact-input-name"]').focus().blur();
     cy.get('[data-cy="contact-input-name"]')
       .parent()
-      .then((el) => {
-        expect(el.attr('class')).to.contains('invalid');
-      });
-    
+      .should('have.attr', 'class').and('match', /invalid/);
+    cy.screenshot();
     cy.get('[data-cy="contact-input-email"]').focus().blur();
     cy.get('[data-cy="contact-input-email"]')
       .parent()
-      .then((el) => {
+      .should((el) => {
         expect(el.attr('class')).to.contains('invalid');
       });
 
