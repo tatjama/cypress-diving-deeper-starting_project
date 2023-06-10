e7cd711c05269fbfd997871f921c5dcd4bfd0c7d
Tanja <tanja120a@gmail.com>
2023-06-10 22:23:58 +0200

    Section03-38 - Simulating special key enter pressed

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -5,16 +5,16 @@ describe('contact form', () => {
         cy.visit('http://localhost:5173/about');
         cy.get('[data-cy="contact-input-message"]').type('Hello World!');
         cy.get('[data-cy="contact-input-name"]').type('tanja');
-        cy.get('[data-cy="contact-input-email"]').type('tanja120a@gmail.com');
         cy.get('[data-cy="contact-btn-submit"]').then((el) => {
         expect(el.attr('disabled')).to.be.undefined;
         expect(el.text()).to.eq('Send Message');
         });
+        cy.get('[data-cy="contact-input-email"]').type('tanja120a@gmail.com{enter}');
         //cy.get('[data-cy="contact-btn-submit"]')
         //.contains('Send Message')
         //.and('not.have.attr', 'disabled');
-        const btn = cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
-        cy.get('@submitBtn').click()
+        cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+        //cy.get('@submitBtn').click()
         cy.get('@submitBtn').contains('Sending...')
         cy.get('@submitBtn').should('have.attr', 'disabled');        
     });
